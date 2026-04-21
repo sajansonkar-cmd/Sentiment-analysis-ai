cf6bb51e296e90763257cd00ff9d63679f324cac
Sarjan Sonkar <sajan.sonkar.797@gmail.com>
2026-04-22 05:16:35 +0530

    changed the sentiments

--- sentiment.py
+++ sentiment.py
@@ -1,11 +1,34 @@
-from textblob import TextBlob
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import os
+
+model = None
+vectorizer = None
+
+def train_model():
+    global model, vectorizer
+
+    path = os.path.join("data", "reviews.csv")
+
+    df = pd.read_csv(path)
+
+    X = df["review"]
+    y = df["sentiment"]
+
+    vectorizer = TfidfVectorizer(stop_words='english')
+    X_vec = vectorizer.fit_transform(X)
+
+    model = LogisticRegression()
+    model.fit(X_vec, y)
 
 def get_sentiment(text):
-    analysis = TextBlob(text)
-    
-    if analysis.sentiment.polarity > 0:
-        return "Positive 😊"
-    elif analysis.sentiment.polarity < 0:
-        return "Negative 😡"
-    else:
-        return "Neutral 😐"
+    global model, vectorizer
+
+    if model is None:
+        train_model()
+
+    text_vec = vectorizer.transform([text])
+    prediction = model.predict(text_vec)[0]
+
+    return prediction
