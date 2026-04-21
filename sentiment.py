c455cb53f987644384c26191d050e174ecd6759a
Sarjan Sonkar <sajan.sonkar.797@gmail.com>
2026-04-22 04:48:30 +0530

    sentiment improve

--- sentiment.py
+++ sentiment.py
@@ -1,12 +1,11 @@
-
 from textblob import TextBlob
 
 def get_sentiment(text):
     analysis = TextBlob(text)
     
     if analysis.sentiment.polarity > 0:
-        return "Positive"
+        return "Positive 😊"
     elif analysis.sentiment.polarity < 0:
-        return "Negative"
+        return "Negative 😡"
     else:
-        return "Neutral"
+        return "Neutral 😐"
